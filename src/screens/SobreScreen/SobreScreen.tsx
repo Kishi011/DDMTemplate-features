53bf65b683bedeb3f5079ae4d967254f5cb2b415
gustavo <gugumund121@gmail.com>
2025-08-27 21:32:57 -0300

    easteregg

--- src/screens/SobreScreen/SobreScreen.tsx
+++ src/screens/SobreScreen/SobreScreen.tsx
@@ -2,10 +2,13 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { PageTemplate } from '@/src/templates';
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Pressable, Linking } from 'react-native';
 
 export const SobreScreen: React.FC = () => {
-  
+
+  const handleOpenLink = () => {
+    Linking.openURL('https://www.youtube.com/watch?v=xvFZjo5PgG0&list=RDxvFZjo5PgG0&start_radio=1'); // substitua pelo link desejado
+  };
 
   return (
     <PageTemplate
@@ -14,14 +17,18 @@ export const SobreScreen: React.FC = () => {
       keyboardAvoiding
     >
       <ThemedView style={styles.header}>
-        
         <ThemedText style={styles.subtitle}>
           Esta é uma aplicação da disciplina de desenvolvimento mobile criada em sala de aula para controle de
           tarefas gerais.
         </ThemedText>
+
+        <Pressable style={styles.button} onPress={handleOpenLink}>
+          <ThemedText style={styles.buttonText}>Para mais informações clique aqui</ThemedText>
+        </Pressable>
       </ThemedView>
     </PageTemplate>
   );
+  
 };
 
 const styles = StyleSheet.create({
@@ -35,5 +42,16 @@ const styles = StyleSheet.create({
     color: '#666',
     textAlign: 'center',
     marginTop: 8,
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: '#A020F0',
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
   },
 });
